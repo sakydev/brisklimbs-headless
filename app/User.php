10316ad538c66977a8b4f4acf500e3460b7f5528
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-06-13 01:23:05 +0500

    User history model added User relationships added

--- app/User.php
+++ app/User.php
@@ -6,35 +6,47 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Video;
+use App\History;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasApiTokens;
+  use Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function videos()
+  {
+    return $this->hasMany(Video::class);
+  }
+
+  public function watched()
+  {
+    return $this->hasMany(UserHistory::class);
+  }
 }
