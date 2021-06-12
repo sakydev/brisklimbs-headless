56b13d72b623c54d229be2133f4cc4e627a65afc
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-06-12 15:09:44 +0500

    Helpers added Video model added AppServiceProvider default key length updated

---
+++ app/Helpers/common.php
@@ -0,0 +1,25 @@
+<?php
+
+function pr($a) {
+	echo '<pre>';
+	print_r($a);
+	echo '</pre>';
+}
+
+function pex($a) {
+	pr($a);
+	exit('Pex');
+}
+
+function api_response($data = array(), $status = 200, $message = null) {
+	$type = strstr($message, 'error:') ? 'error' : 'message';
+	return response()->json([
+		$type => $message,
+		'status' => $status,
+		'data' => $data
+	], $status);
+}
+
+function api_response_message($message, $status) {
+	return api_response(false, $status, $message);
+}
