fc798a412dd8a0c78464b651ca0b54311219dc3a
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-06-16 01:12:36 +0500

    Categories endpoint finished

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Video;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'description'];
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
 }
