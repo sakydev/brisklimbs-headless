122a1c978aa5518dbe92e69aa87d819c402eb6b8
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-06-16 01:10:37 +0500

    Admin middleware added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,83 +3,123 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Video;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  protected function validator($validator)
+  {
+    if ($validator->fails()) {
+      $errors = $validator->messages()->get('*');
+      return api_response($errors, 422, 'error: incomplete / invalid input');
+    }
+  }
+
+  protected function category_validation_fails($data)
+  {
+    $validator = Validator::make($data, [
+      'name' => 'required|string|unique:categories'
+    ]);
+
+    return $this->validator($validator);
+  }
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $results = Category::all();
+    return api_response($results, 200);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $category_validation_fails = $this->category_validation_fails($request->all());
+    if ($category_validation_fails) {
+      return $category_validation_fails;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    $created = Category::create([
+      'name' => $request->name,
+      'description' => $request->description
+    ]);
+
+    return api_response($created, 201, 'Category created successfully');
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function show($category)
+  {
+    $category = Category::find($category);
+    return api_response($category, 200);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $category)
+  {
+    $category = Category::find($category);
+    if (!$category) {
+      return api_response_error('Category not found', 404);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    $category_validation_fails = $this->category_validation_fails($request->all());
+    if ($category_validation_fails) {
+      return $category_validation_fails;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
+    if ($category->name != $request->name) {
+      $category->name = $request->name;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
+    if ($request->description) {
+      $category->description = $request->description;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
+    $category->save();
+
+    return api_response($category, 200, 'Category updated');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Category  $category
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($category)
+  {
+    $category = Category::find($category);
+    if (!$category) {
+      return api_response_error('Category not found', 404);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
-    {
-        //
-    }
+    // first update all videos with this category to uncategorized
+    $videos = $category->videos()->update(['category_id' => 1]);
+    $category->delete();
+
+    return api_response_message('Category deleted', 200);
+  }
 }
